ce016675385945f83ad514ac3378026874d2c4d0
John Palevich <john.palevich@gmail.com>
2021-12-02 21:49:03 -0800

    Day 3, This one was hard, spent a lot of time trying to figure out a good strategy of determining whether there were more 0s or 1s

--- day3.py
+++ day3.py
@@ -1,3 +1,5 @@
+
+
 test = "testInput/"
 real = "input/"
 day = "day3.txt"
@@ -10,10 +12,37 @@ def getInput(test):
     return a
 
 def part1(a):
-    return
-
+    gamma = ''
+    epsilon = ''
+    tot = []
+    for i in range(len(a[0])):
+        tot.append(sum([int(l[i]) for l in a]))
+    for v in tot:
+        gamma = gamma + '1' if int(v)>len(a)/2 else gamma+'0'
+        epsilon = epsilon + '0' if int(v) > len(a)/2 else epsilon+'1'
+    return int(gamma,2) * int(epsilon,2)
+ 
 def part2(a):
-    return
+    predOxy = ''
+    predCO2 = ''
+    possOxyVals = a
+    possCO2Vals = a
+    for i in range(len(a[0])):
+        totOxy = sum([int(l[i]) for l in possOxyVals])
+        totCO2 = sum([int(l[i]) for l in possCO2Vals])
+        predOxy = predOxy + '1' if totOxy >= len(possOxyVals)/2 else predOxy + '0'
+        predCO2 = predCO2 + '0' if totCO2 >= len(possCO2Vals)/2 else predCO2 + '1'
+        newPossOxyVals = [] if len(possOxyVals) > 1 else possOxyVals
+        newPossCO2Vals = [] if len(possCO2Vals) > 1 else possCO2Vals
+        for v in possOxyVals:
+            if v.startswith(predOxy):
+                newPossOxyVals.append(v)
+        for v in possCO2Vals:
+            if v.startswith(predCO2):
+                newPossCO2Vals.append(v)
+        possOxyVals = newPossOxyVals
+        possCO2Vals = newPossCO2Vals
+    return int(possOxyVals[0],2) * int(possCO2Vals[0],2)
 
 print(part1(getInput(test + day)))
 print(part1(getInput(real + day)))
